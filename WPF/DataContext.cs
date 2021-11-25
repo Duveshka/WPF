bb7e95cd629fb2120d84053b39c5eedd9cdc1f1a
duveshka <duveshka@mail.ru>
2021-11-26 02:44:57 +0300

    Add changes

---
+++ WPF/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF
+{
+    class DataContext
+    {
+        public string Name { get; set; }
+        public string Focus { get; set; }
+        public string Bla { get; set; }
+    }
+}
